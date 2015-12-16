2ff59354ddfebc1aa0eba3aa838f4d621c9796d9
Magnus Fjell <magnus.fjell@gmail.com>
2015-12-16 13:02:03 +0100

    Remove logging and temp data

--- index.js
+++ index.js
@@ -4,39 +4,20 @@ var path = require('path');
 var config = require('./config.json');
 var ocr = require('./ocr');
 
-var data = {
-  "image": "http://image.tld/asdasweadas.jpg",
-  "tags": [
-    "Bacon",
-    "Øl"
-  ],
-  "characters": [
-    "Kjell",
-    "Bache"
-  ],
-  "ocrtext": "teksten skal inn her",
-  "correctedtext": "dette kan være manuelt korrigert tekst, originalt samme som ocrtext"
-};
-
 var esDomain = {
   endpoint: config.esUrl,
   region: 'eu-west-1',
   index: 'comics'
 };
+
 var endpoint =  new AWS.Endpoint(esDomain.endpoint);
 var creds = new AWS.EnvironmentCredentials('AWS');
 var s3 = new AWS.S3();
 
 exports.handler = function (event, context) {
-  console.log('node version', process.version); // v0.10.36
-  console.log('got s3 event', JSON.stringify(event, null, 2));
-  console.log('works');
-
   event.Records.forEach(function (record) {
     var bucket = record.s3.bucket.name;
     var key = decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '));
-    console.log('bucket', bucket,
-                'object key', key);
 
     var imageStream = s3.getObject({ Bucket: bucket, Key: key }).createReadStream();
 
