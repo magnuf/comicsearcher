48cafad0e022b50195e46495f0d9f407579a4614
Magnus Fjell <magnus.fjell@gmail.com>
2015-12-16 11:19:56 +0100

    Remove unneeded code from ocr

--- ocr.js
+++ ocr.js
@@ -41,65 +41,12 @@ function authorize(credentials) {
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      authClient = oauth2Client;
-      EventEmitter.emit("authed")
-    }
+    oauth2Client.credentials = JSON.parse(token);
+    authClient = oauth2Client;
+    EventEmitter.emit("authed")
   });
 }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-function getNewToken(oauth2Client) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
-      }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      authClient = oauth2Client;
-      EventEmitter.emit("authed")
-    });
-  });
-}
-
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-function storeToken(token) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
-    }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + TOKEN_PATH);
-}
 
 function uploadFile(path, callback) {
   var drive = google.drive({ version: 'v2', auth: authClient });
