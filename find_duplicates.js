1bdc4b005c16ac7d68dda81135b4dda515cdf749
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2016-01-06 22:56:51 +0100

    omfg, resemble? resemblejs? resemble.js? node-resemble? node-resemble-js? node-resemble.js?

--- find_duplicates.js
+++ find_duplicates.js
@@ -1,37 +1,54 @@
-var resemble = require("node-resemble-js");
+var resemble = require("resemble").resemble;
 var fs = require("fs");
 var Seq = require("seq");
 
 fs.readdir("comics", function(err, files) {
-	var matches = []
-	if (err) { return; }
-	for (var i =  0; i < files.length; i++) {
-		if(files[i].endsWith(".gif")) { continue; }
-			matches.push([files[i], files.filter(function(file) {
-				return file != files[i] && file.endsWith(".png");
-			})]);
-	}
+  var matches = []
+  if (err) { return; }
+  for (var i =  0; i < files.length; i++) {
+    if(files[i].endsWith(".gif")) { continue; }
+    matches.push([files[i], files.filter(function(file) {
+      return file != files[i] && file.endsWith(".png");
+    })]);
+  }
 
-	Seq(matches)
-		.seqEach(function(tuple) {
-			var outerSeq = this;
-			Seq(tuple[1])
-				.seqEach(function(filename) {
-					var cb = this.into(tuple[0] + filename);
-					var diff = resemble("comics/"+tuple[0]).compareTo("comics/"+filename).ignoreColors().onComplete(function(data) { console.log("one down"); cb(null, data);});
-				}).seq(function() {
-					console.log(this.vars);
-					this.ok();
-						// if (this.data.misMatchPercentage < 25) {
-						// 	console.log("%s is duplicate of %s", files[j], files[i]);
-						// }
-						// console.log(data.misMatchPercentage);
-						// this.ok();
-				})
-				.seq(function(){ 				console.log("hei1"); outerSeq.ok()})
-				// .catch(console.log.bind(console));
-				.catch(function(err) {console.log("hei") });
-		})
-.catch(function() {console.log("hei2") });
-		// .catch(console.log.bind(console));
+  Seq(matches)
+    .seqEach(function(tuple) {
+      var outerSeq = this;
+      Seq(tuple[1])
+        .seqEach(function(filename) {
+          var key = tuple[0] + filename;
+          var cb = this.into(key);
+          var path = "comics/" + tuple[0],
+              comparePath = "comics/" + filename;
+          console.log('running', path, "comparing to", comparePath);
+          resemble(path)
+            .compareTo(comparePath)
+            .ignoreColors()
+            .onComplete(function(data) {
+              console.log(data);
+              setTimeout(function () {
+                cb(null, data);
+              }, 0);
+            });
+        })
+        .catch(function (err) {
+          console.log('catch for seqEach', err);
+        })
+        .seq(function() {
+          console.log('ok');
+          console.log(this.vars);
+          // if (this.data.misMatchPercentage < 25) {
+          //      console.log("%s is duplicate of %s", files[j], files[i]);
+          // }
+          // console.log(data.misMatchPercentage);
+          outerSeq.ok();
+        })
+        .catch(function (err) {
+          console.log("err inner", err);
+        });
+    })
+    .catch(function (err) {
+      console.log("err outer", err)
+    });
 });
