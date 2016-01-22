b7f2e5dd0085e7f13b4aa40ddc32a0fdde51011d
Magnus Fjell <magnus.fjell@gmail.com>
2016-01-22 21:12:22 +0100

    Make a running example if you have everything installed

--- find_duplicates.js
+++ find_duplicates.js
@@ -13,44 +13,42 @@ fs.readdir("testcomics", function(err, files) {
   }
 
   var minWidth = 999999999999;
+
   im.identify('testcomics/'+matches[0][0], function(err, features){
-      if (err) throw err;
+    minWidth = Math.min(features.width, minWidth);
+
+    im.identify('testcomics/'+matches[0][1][0], function(err, features){
       minWidth = Math.min(features.width, minWidth);
       // { format: 'JPEG', width: 3904, height: 2622, depth: 8 }
+
+      resizeOptions1 = {
+        srcPath: 'testcomics/'+matches[0][0],
+        dstPath: 'temp1.png',
+        width:   minWidth,
+        format: 'png'
+      };
+
+      resizeOptions2 = {
+        srcPath: 'testcomics/'+matches[0][1][0],
+        dstPath: 'temp2.png',
+        width:   minWidth,
+        format: 'png'
+      };
+
+      im.resize(resizeOptions1, function(err, stdout, stderr) {
+        console.log("resized 1");
+        im.resize(resizeOptions2, function(err, stdout, stderr) {
+          console.log("resized 2");
+          resemble("temp1.png")
+          .compareTo("temp2.png")
+          .ignoreColors()
+          .onComplete(function(data) {
+            console.log(data);
+          });
+        });
+      });
     });
-
-  im.identify('testcomics/'+matches[0][1][0], function(err, features){
-      if (err) throw err;
-      minWidth = Math.min(features.width, minWidth);
-      // { format: 'JPEG', width: 3904, height: 2622, depth: 8 }
-    });
-
-  resizeOptions1 = {
-      srcPath: 'testcomics/'+matches[0][0],
-      dstPath: 'temp1.png',
-      width:   minWidth,
-      format: 'png'
-    };
-  im.resize(resizeOptions1, function(err, stdout, stderr) {
-    console.log("resized 1");
   });
-
-  resizeOptions1 = {
-      srcPath: 'testcomics/'+matches[0][1][0],
-      dstPath: 'temp2.png',
-      width:   minWidth,
-      format: 'png'
-    };
-  im.resize(resizeOptions2, function(err, stdout, stderr) {
-    console.log("resized 2");
-  });
-
-  resemble("temp1.png")
-            .compareTo("temp2.png")
-            .ignoreColors()
-            .onComplete(function(data) {
-              console.log(data);
-            });
 
 return;
 
