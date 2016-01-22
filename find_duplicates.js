e42ff48a8ee16239edeb7e2971b138a34ae1cfbe
Magnus Fjell <magnus.fjell@gmail.com>
2016-01-22 21:02:07 +0100

    Setup suggestion for using imagemagick to resize and convert images to the same size before using resemble to diff them

--- find_duplicates.js
+++ find_duplicates.js
@@ -1,16 +1,58 @@
 var resemble = require("resemble").resemble;
 var fs = require("fs");
 var Seq = require("seq");
+var im = require('imagemagick');
 
-fs.readdir("comics", function(err, files) {
+fs.readdir("testcomics", function(err, files) {
   var matches = []
   if (err) { return; }
   for (var i =  0; i < files.length; i++) {
-    if(files[i].endsWith(".gif")) { continue; }
     matches.push([files[i], files.filter(function(file) {
-      return file != files[i] && file.endsWith(".png");
+      return file != files[i];
     })]);
   }
+
+  var minWidth = 999999999999;
+  im.identify('testcomics/'+matches[0][0], function(err, features){
+      if (err) throw err;
+      minWidth = Math.min(features.width, minWidth);
+      // { format: 'JPEG', width: 3904, height: 2622, depth: 8 }
+    });
+
+  im.identify('testcomics/'+matches[0][1][0], function(err, features){
+      if (err) throw err;
+      minWidth = Math.min(features.width, minWidth);
+      // { format: 'JPEG', width: 3904, height: 2622, depth: 8 }
+    });
+
+  resizeOptions1 = {
+      srcPath: 'testcomics/'+matches[0][0],
+      dstPath: 'temp1.png',
+      width:   minWidth,
+      format: 'png'
+    };
+  im.resize(resizeOptions1, function(err, stdout, stderr) {
+    console.log("resized 1");
+  });
+
+  resizeOptions1 = {
+      srcPath: 'testcomics/'+matches[0][1][0],
+      dstPath: 'temp2.png',
+      width:   minWidth,
+      format: 'png'
+    };
+  im.resize(resizeOptions2, function(err, stdout, stderr) {
+    console.log("resized 2");
+  });
+
+  resemble("temp1.png")
+            .compareTo("temp2.png")
+            .ignoreColors()
+            .onComplete(function(data) {
+              console.log(data);
+            });
+
+return;
 
   Seq(matches)
     .seqEach(function(tuple) {
